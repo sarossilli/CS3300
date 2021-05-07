50082b9b47c8131c15e27540b3dda4df526ecded
Sam Rossilli <sarossilli@gmail.com>
2021-05-07 11:26:23 -0600

    Added img for each project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,8 @@ class Project < ApplicationRecord
     extend FriendlyId
     validates_presence_of :title, :description, :short_desc, :demo_video
 
+    has_one_attached :img
+    
     friendly_id :title, use: :slugged  
 
     def should_generate_new_friendly_id?
